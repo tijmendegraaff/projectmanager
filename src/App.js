d8c895d3ac0452ff896cb61c15a627c8015da347
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-19 23:05:14 +0200

    add Project component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>My app</p>
+        My app
+        <Projects />
       </div>
     );
   }
