6d9002ab4588d74620a59f05482a3fa67d59201d
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-19 23:20:07 +0200

    add prop

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         My app
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="Projects">
         My Projects
-        {this.props.test}
       </div>
     );
   }
